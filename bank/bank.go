ee6bf4bcf55edf4d02e6b0f9b6be47a13d5da2d7
GiovanePS <giovanespace@gmail.com>
2024-10-11 15:34:20 -0300

    Independent tests for transferir

--- bank/bank.go
+++ bank/bank.go
@@ -15,38 +15,7 @@ type Node struct {
 	next    *Node
 }
 
-type Account struct {
-	id    int
-	saldo float64
-}
-
-func newAccount(idContador int) *Account {
-	newAccount := &Account{idContador, 0.0}
-	return newAccount
-}
-
-func (a *Account) depositar(valor float64) error {
-	if valor < 0 {
-		return fmt.Errorf("Só é possível realizar depósitos de valores positivos")
-	}
-
-	a.saldo += valor
-	return nil
-}
-
-func (a *Account) sacar(valor float64) error {
-	// absolute value
-	if valor < 0 {
-		valor = -valor
-	}
-
-	if a.saldo < valor {
-		return fmt.Errorf("Saldo insuficiente para sacar R$ %v", valor)
-	}
-
-	a.saldo -= valor
-	return nil
-}
+// TODO: COLOCAR CONTA EM OUTRO ARQUIVO NESSE PACKAGE
 
 func NewBank() *Bank {
 	newBank := &Bank{
--- bank/bank_test.go
+++ bank/bank_test.go
@@ -7,7 +7,7 @@ import (
 func TestGetAccount(t *testing.T) {
 	bank := NewBank()
 
-	t.Run("Testar sucesso ao pegar uma conta", func(t *testing.T) {
+	t.Run("Sucesso ao pegar uma conta", func(t *testing.T) {
 		want := bank.CreateAccount().id
 		account, err := bank.GetAccount(1)
 		if err != nil {
@@ -21,7 +21,7 @@ func TestGetAccount(t *testing.T) {
 		}
 	})
 
-	t.Run("Testar falha ao tentar pegar uma conta inexistente", func(t *testing.T) {
+	t.Run("Falhar ao tentar pegar uma conta inexistente", func(t *testing.T) {
 		_, err := bank.GetAccount(1000)
 		if err == nil {
 			t.Errorf("Erro não retornado ao pegar um usuário inexistente.")
@@ -47,7 +47,7 @@ func TestDepositarOuSacar(t *testing.T) {
 		}
 	})
 
-	t.Run("Falha ao depositar em uma conta que não existe", func(t *testing.T) {
+	t.Run("Falhar ao depositar em uma conta que não existe", func(t *testing.T) {
 		err := bank.DepositarOuSacar(1000, 0)
 		if err == nil {
 			t.Errorf("Erro não retornado em uma operação inválida.")
@@ -68,7 +68,7 @@ func TestDepositarOuSacar(t *testing.T) {
 		}
 	})
 
-	t.Run("Falha ao sacar um valor maior do que o disponível na conta", func(t *testing.T) {
+	t.Run("Falhar ao sacar um valor maior do que o disponível na conta", func(t *testing.T) {
 		err := bank.DepositarOuSacar(1, -1000)
 		if err == nil {
 			t.Errorf("Erro não retornado ao sacar um valor maior do que o disponível na conta.")
@@ -77,33 +77,53 @@ func TestDepositarOuSacar(t *testing.T) {
 }
 
 func TestTransferir(t *testing.T) {
-	t.Run("Sucesso ao transferir", func(t *testing.T) {
-		bank := NewBank()
-		acc1 := bank.CreateAccount()
+	setup := func() (bank *Bank, acc1 *Account, acc2 *Account) {
+		bank = NewBank()
+		acc1 = bank.CreateAccount()
 		err := bank.DepositarOuSacar(1, 10)
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		acc2 := bank.CreateAccount()
+		acc2 = bank.CreateAccount()
 		err = bank.DepositarOuSacar(2, 10)
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		bank.Transferir(1, 2, 5)
-		got := acc1.saldo
-		want := 5.0
+		return bank, acc1, acc2
+	}
 
-		if got != want {
-			t.Errorf("Saldo do remetente da transferência incorreto: got: %f, want: %f", got, want)
+	checkErrors := func(t *testing.T, sourceGot float64, sourceWant float64, destGot float64, destWant float64) {
+		if sourceGot != sourceWant {
+			t.Errorf("Saldo do remetente da transferência incorreto: got: %f, want: %f", sourceGot, sourceWant)
 		}
 
-		got = acc2.saldo
-		want = 15.0
+		if destGot != destWant {
+			t.Errorf("Saldo do destinatário da transferência incorreto: got: %f, want: %f", destGot, destWant)
+		}
+	}
 
-		if got != want {
-			t.Errorf("Saldo do destinatário da transferência incorreto: got: %f, want: %f", got, want)
+	t.Run("Sucesso ao transferir", func(t *testing.T) {
+		bank, acc1, acc2 := setup()
+		err := bank.Transferir(1, 2, 5)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		sourceGot := acc1.saldo
+		sourceWant := 5.0
+		destGot := acc2.saldo
+		destWant := 15.0
+
+		checkErrors(t, sourceGot, sourceWant, destGot, destWant)
+	})
+
+	t.Run("Falhar ao transferir com conta destino sem saldo suficiente", func(t *testing.T) {
+		bank, _, _ := setup()
+		err := bank.Transferir(1, 2, 15) // Saldo insuficiente para source
+		if err == nil {
+			t.Errorf("Erro não retornado ao sacar um valor maior do que o disponível na conta.")
 		}
 	})
 }
